d139085bc84ca0c6532809773066f3c59b57b464
Adam4654 <144643122+Adam4654@users.noreply.github.com>
2024-10-12 14:36:34 +0800

    Update main.cpp
    version 0.3 12 oct. 2024 8:30

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include "tp3.h"
 
 int main() {
-    //========Initialisation=========//
-    matrice_creuse *matrice1;
-    matrice_creuse matrice2;
+    /* ============= variable de utilisateur ============= */
+    struct MatriceCreuse* m1;
+    struct MatriceCreuse* m2;
     // ============= MENU UTILISATEUR ============= */
     char choix = '0';
     while (choix != '8') {
@@ -15,7 +15,7 @@ int main() {
         printf("\n2. Afficher une matrice creuse sous forme de tableau");
         printf("\n3. Afficher une matrice creuse sous forme de listes");
         printf("\n4. Donner la valeur d'un element d'une matrice creuse");
-        printf("\n5. Affecter une valeur à un élément d’une matrice creuse");
+        printf("\n5. Affecter une valeur ? un 閘閙ent d抲ne matrice creuse");
         printf("\n6. Additionner deux matrices creuses");
         printf("\n7. Calculer le gain");
         printf("\n8. Quitter");
@@ -25,14 +25,11 @@ int main() {
 
         switch (choix) {
             case '1' :
-                matrice1 = cree_matrice();
-                remplirMatrice(&matrice2, 4, 2);
-                // Ecrire ici le code pour ce choix utlisateur
+                m1=creerMatrice();
+                remplirMatrice(m1,m1->Nlignes,m1->Ncolonnes);
                 break;
 
             case '2' :
-                afficherMatrice(*matrice1);
-                afficherMatrice(matrice2);
                 // Ecrire ici le code pour ce choix utlisateur
                 break;
 
@@ -54,12 +51,9 @@ int main() {
 
             case '7' :
                 // Ecrire ici le code pour ce choix utlisateur
-               break;
+                break;
 
             case '8' :
-                free_matrice(*matrice1);
-                free(matrice1);
-                free_matrice(matrice2);
                 printf("\n======== PROGRAMME TERMINE ========\n");
                 break;
 
